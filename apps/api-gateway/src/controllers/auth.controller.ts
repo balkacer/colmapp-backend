3033d7cca412fde209407585b7e68a0f08b4781e
Enmanuel Balcacer <balcacer.enmanuel@gmail.com>
2025-09-18 18:04:40 -0400

    feat: refactor AuthController for api-gateway

--- apps/api-gateway/src/controllers/auth.controller.ts
+++ apps/api-gateway/src/controllers/auth.controller.ts
@@ -1,17 +1,24 @@
 import { Controller, Post, Body, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom, retry, timeout } from 'rxjs';
 
 @Controller('auth')
 export class AuthController {
-  constructor(@Inject('AUTH_SERVICE') private authClient: ClientProxy) {}
+  constructor(@Inject('AUTH_SERVICE') private authClient: ClientProxy) { }
 
   @Post('register')
   register(@Body() dto: any) {
-    return this.authClient.send({ cmd: 'register' }, dto);
+    return lastValueFrom(this.authClient.send({ cmd: 'register' }, dto).pipe(
+      timeout(10000),
+      retry(3),
+    ));
   }
 
   @Post('login')
   login(@Body() dto: any) {
-    return this.authClient.send({ cmd: 'login' }, dto);
+    return lastValueFrom(this.authClient.send({ cmd: 'login' }, dto).pipe(
+      timeout(10000),
+      retry(3),
+    ));
   }
 }
