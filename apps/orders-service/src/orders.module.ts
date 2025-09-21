a75673e66844cda20679d41520e513924469b7be
Enmanuel Balcacer <balcacer.enmanuel@gmail.com>
2025-09-21 13:13:17 -0400

    fix: adding missing OrderModule's Mongo config

--- apps/orders-service/src/orders.module.ts
+++ apps/orders-service/src/orders.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { configuration, validationSchema } from '@colmapp/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: [configuration],
+    validationSchema,
+  }),
+  MongooseModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (config: ConfigService) => ({
+      uri: config.get<string>('mongoUri'),
+      dbName: config.get<string>('mongoDbName')
+    }),
+  }),
+  MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }])],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
-export class OrdersModule {}
+export class OrdersModule { }
