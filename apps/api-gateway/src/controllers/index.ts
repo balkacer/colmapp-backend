7f172ffe38d87e1e193110a6317cf17009c809d4
Enmanuel Balcacer <balcacer.enmanuel@gmail.com>
2026-01-21 06:13:59 -0400

    feat: add public access to status endpoint and include users and business controllers in exports

--- apps/api-gateway/src/controllers/index.ts
+++ apps/api-gateway/src/controllers/index.ts
@@ -3,4 +3,6 @@ export * from './orders.controller';
 export * from './products.controller';
 export * from './providers.controller';
 export * from './customers.controller';
+export * from './users.controller';
+export * from './business.controller';
 // Add other controllers here as needed
